f3f6c1802c275c1a8f87104e224d4cb750180ac1
Artur Korop <rubkaarchi@gmail.com>
2015-03-13 13:27:24 +0200

    Bolt

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float Speed;
+
+    public void Start()
+    {
+        var rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.velocity = this.transform.forward * this.Speed;
+    }
+}
