e70ca4e90ce50a35aac71123d2c745499eae194f
Artur Korop <rubkaarchi@gmail.com>
2015-03-19 17:40:13 +0200

    Asteroid behavior

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject Explosion;
+    public GameObject PlayerExplosion;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+
+        Instantiate(this.Explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(this.PlayerExplosion, other.transform.position, other.transform.rotation);
         }
 
         Destroy(other.gameObject);
