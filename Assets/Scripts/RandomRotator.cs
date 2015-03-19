40da026f984a54db347e3a5a3fa386a1c055b350
Artur Korop <rubkaarchi@gmail.com>
2015-03-19 09:57:32 +0200

    Asteroid

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+    public float Tumble;
+
+    public void Start()
+    {
+        var rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Random.insideUnitSphere * this.Tumble;
+    }
+
+}
