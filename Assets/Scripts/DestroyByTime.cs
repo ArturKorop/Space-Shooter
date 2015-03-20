3cd736cd14da97a0b36497dbd5113e848ddd49ee
Artyr <rubkaarchi@gmail.com>
2015-03-20 22:41:02 +0200

    Game Controller

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float LifeTime;
+
+    public void Start()
+    {
+        Destroy(gameObject, this.LifeTime);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Hazard;
+    public Vector3 SpawnValue;
+    public int HazardCount;
+    public float SpawnWait;
+    public float StartWait;
+    public float WaveWait;
+
+    public void Start()
+    {
+        StartCoroutine(this.SpawnWaves());
+    }
+
+    private IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(this.StartWait);
+
+        while (true)
+        {
+            for (int i = 0; i < this.HazardCount; i++)
+            {
+                var spawnPosition = new Vector3(Random.RandomRange(-this.SpawnValue.x, this.SpawnValue.x), this.SpawnValue.y, this.SpawnValue.z);
+                var spawnRotation = Quaternion.identity;
+                Instantiate(this.Hazard, spawnPosition, spawnRotation);
+
+                yield return new WaitForSeconds(this.SpawnWait);
+            }
+
+            yield return new WaitForSeconds(this.WaveWait);
+        }
+    }
+}
